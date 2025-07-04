c4f117ff04dc1aefbcd1cdd168a2b178d8f8a227
Faique <faique.official695@gmail.com>
2025-07-04 13:24:04 +0530

    set up ho gya

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+import { app } from "./app.js";
+
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
 .then(() => {
-   app.listen(process.env.PORT || 8000, () => {
+   app.listen(process.env.PORT , () => {
         console.log(`Server is running on port ${process.env.PORT}`);
     });
 })
 .catch((error) => {
     console.error("Mongo DB connection nahi hua", error);
-    // process.exit(1);  Exit the process with failure
+    // process.exit(1);  //Exit the process with failure
 });
 
 
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
     constructor( statusCode,
         message= "Something went wrong",
         errors = [],
-        statck = "") {
+        stack = "") {
         super(message)
         this.data = null
         this.statusCode = statusCode;
@@ -11,8 +11,8 @@ class ApiError extends Error {
         this.success = false;
         this.message = message;
 
-        if(statck) {
-            this.stack = statck;
+        if(stack) {
+            this.stack = stack;
         }
         else {
             Error.captureStackTrace(this, this.constructor);
